6e0b94807aeb6a0fa02fa0264a47cede3d175f18
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-06-10 14:04:53 +0100

    Feat: Improve display fun, create operator catcher
    - Created event listeners on operator buttons.
    - Display function now has size limit and improved 0 button
      compatability.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let displayNumber = ["0","0"];
+let displayNumberSelector = 0;
+let selectedOperator;
+
+updateDisplay();
+
 function add(a, b) {
 	return a + b;
 }
@@ -14,18 +20,55 @@ function divide(a, b) {
 	return a / b;
 }
 
-let firstNumber, secondNumber, operator;
 
-function operate(a, b, operator) {
-	return operator(a, b);
+function operate(a, b, op) {
+	return op(a, b);
 }
 
-function updateDisplay(number) {
-	document.querySelector("#display").textContent += number;
+function updateDisplay(number = "") {
+	let displayLength = displayNumber[displayNumberSelector].length;
+
+	// Check display has space and non empty
+	if (displayLength < 8 && (!(number === "0") || displayLength > 0)) {
+
+		// Check display is not 0 before appending digits
+		if (!(number === "") && displayNumber[displayNumberSelector] === "0") {
+			displayNumber[displayNumberSelector] = number;
+		} else {
+			displayNumber[displayNumberSelector] += number;
+		}
+		document.querySelector("#display").textContent = displayNumber[displayNumberSelector];
+	}
+}
+
+function equals() {
+	//Assume displayNumber is filled
 }
 
 
 let btnNumbers = document.querySelectorAll(".number");
 
 btnNumbers.forEach( number => {
-	number.addEventListener('click', () => updateDisplay(number.textContent))});
+	number.addEventListener("click", () => updateDisplay(number.textContent))});
+
+let btnOperators = document.querySelectorAll(".operator");
+
+btnOperators.forEach( operator => {
+	operator.addEventListener("click", () => {
+		switch(operator.textContent) {
+			case "+":
+				selectedOperator = add;
+				break;
+			case "−":
+				selectedOperator = subtract;
+				break;
+			case "×":
+				selectedOperator = multiply;
+				break;
+			case "÷":
+				selectedOperator = divide;
+				break;
+		}
+		displayNumberSelector = 1;
+	})
+});
