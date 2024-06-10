78d9b980bc5002cad0c4e4cf607fa32c344a667d
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-06-10 17:42:06 +0100

    Feat: AC Button |Fix: updateDisplay
    -Bug on updateDisplay function causing results of length 8 not to show
    fixed.

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
 let displayNumber = ["0","0"];
-let dNS = 1; //displayNumberSelector
+let dNP = 1; //displayNumberPointer
 let selectedOperator;
 
 updateDisplay();
+
+let btnNumbers = document.querySelectorAll(".number");
+let btnEquals = document.querySelector("#equals");
+let btnAllClear = document.querySelector("#all-clear");
+let btnOperators = document.querySelectorAll(".operator");
+
+btnNumbers.forEach( number => {
+	number.addEventListener("click", () => updateDisplay(number.textContent))});
+
+btnEquals.addEventListener("click", equals);
+
+btnAllClear.addEventListener("click", () => {
+	displayNumber = ["0", "0"];
+	dNP = 1;
+	selectedOperator = undefined;
+	updateDisplay();
+});
+
+btnOperators.forEach( operator => {
+	operator.addEventListener("click", () => {
+
+		if (displayNumber[dNP] === "error") return;
+
+		// Using operator instead of equals
+		if (dNP === 1 && selectedOperator) {
+			equals();
+			if (displayNumber[dNP] === "error") return;
+		} 
+		// First time pressing the operator button
+		else if (dNP === 1) {
+			displayNumber[0] = displayNumber[1];
+		}
+
+		switch(operator.textContent) {
+			case "+":
+				selectedOperator = add;
+				break;
+			case "−":
+				selectedOperator = subtract;
+				break;
+			case "×":
+				selectedOperator = multiply;
+				break;
+			case "÷":
+				selectedOperator = divide;
+				break;
+		}
+		dNP = 1;
+		displayNumber[dNP] = "0";
+	})
+});
+
+
 
 function add(a, b) {
 	return a + b;
@@ -26,22 +79,23 @@ function operate(a, b, op) {
 }
 
 function updateDisplay(number = "") {
-	if (displayNumber[dNS] === "error") {
-		document.querySelector("#display").textContent = displayNumber[dNS];
+	if (displayNumber[dNP] === "error") {
+		document.querySelector("#display").textContent = displayNumber[dNP];
 		return;
 	}
-	let displayLength = displayNumber[dNS].length;
+	let displayLength = displayNumber[dNP].length;
 
 	// Check display has space and non empty
-	if (displayLength < 8 && (!(number === "0") || displayLength > 0)) {
+	if ((displayLength < 8 && (!(number === "0") || displayLength > 0))
+		|| (displayLength === 8 && number === "")) {
 
 		// Check display is not 0 before appending digits
-		if (!(number === "") && displayNumber[dNS] === "0") {
-			displayNumber[dNS] = number;
+		if (!(number === "") && displayNumber[dNP] === "0") {
+			displayNumber[dNP] = number;
 		} else {
-			displayNumber[dNS] += number;
+			displayNumber[dNP] += number;
 		}
-		document.querySelector("#display").textContent = displayNumber[dNS];
+		document.querySelector("#display").textContent = displayNumber[dNP];
 	}
 }
 
@@ -53,53 +107,7 @@ function equals() {
 	let a, b;
 	[a, b] = displayNumber;
 	let result = selectedOperator(+a, +b).toString();
-	dNS = 0;
-	displayNumber[dNS] = result.length > 8 ? "error" : result;
+	dNP = 0;
+	displayNumber[dNP] = result.length > 8 ? "error" : result;
 	updateDisplay();
 }
-
-
-let btnNumbers = document.querySelectorAll(".number");
-
-btnNumbers.forEach( number => {
-	number.addEventListener("click", () => updateDisplay(number.textContent))});
-
-let btnOperators = document.querySelectorAll(".operator");
-
-btnOperators.forEach( operator => {
-	operator.addEventListener("click", () => {
-
-		if (displayNumber[dNS] === "error") return;
-
-		// Using operator instead of equals
-		if (dNS === 1 && selectedOperator) {
-			equals();
-			if (displayNumber[dNS] === "error") return;
-		} 
-		// First time pressing the operator button
-		else if (dNS === 1) {
-			displayNumber[0] = displayNumber[1];
-		}
-
-		switch(operator.textContent) {
-			case "+":
-				selectedOperator = add;
-				break;
-			case "−":
-				selectedOperator = subtract;
-				break;
-			case "×":
-				selectedOperator = multiply;
-				break;
-			case "÷":
-				selectedOperator = divide;
-				break;
-		}
-		dNS = 1;
-		displayNumber[dNS] = "0";
-	})
-});
-
-let btnEquals = document.querySelector(".equals");
-
-btnEquals.addEventListener("click", equals);
